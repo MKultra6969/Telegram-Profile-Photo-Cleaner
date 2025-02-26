61d9b4f3c8488b48ab277cb534b08ece0c056f61
MKultra <mkultra69@otso.city>
2025-02-26 06:48:44 +0500

    Minor upd
    Функция выхода в меню путем отправки 'q'
    Более душевный текст, сосмыслом и душой
    Чуть чуть переделана обработка ошибки юзер инпута

--- TPPC.py
+++ TPPC.py
@@ -20,89 +20,89 @@ def delete_all_photos():
     try:
         user = client.get_me()
 
-        # Получаем аватарки профиля пользователя
         photos = list(client.get_chat_photos(user.id))
 
         if not photos:
-            logger.info("Нет аватарок для удаления.")
+            logger.info("Нет нихуя, нечего удалять.")
             return
 
         for photo in photos:
             try:
                 client.delete_profile_photos(photo.file_id)
-                logger.info(f"Аватарка {photo.file_id} удалена успешно.")
+                logger.info(f"Авка {photo.file_id} удалена.")
             except FloodWait as e:
-                logger.warning(f"Превышен лимит запросов. Подождите {e.x} секунд.")
+                logger.warning(f"Флуд. Ждем {e.x} секунд.")
             except Exception as e:
-                logger.error(f"Ошибка при удалении аватарки {photo.file_id}: {e}")
+                logger.error(f"НЕ СМОГ УДАЛИТЬ ЧЗХ??? {photo.file_id}: {e}")
 
     except Exception as e:
-        logger.error(f"Ошибка при получении аватарок профиля: {e}")
+        logger.error(f"ОШИБКА при получении авок профиля: {e}")
 
 
 def delete_some_photos(count):
     try:
         user = client.get_me()
 
-        # Получаем аватарки профиля пользователя
         photos = list(client.get_chat_photos(user.id))
 
         if not photos:
-            logger.info("Нет аватарок для удаления.")
+            logger.info("Нет нихуя, нечего удалять.")
             return
 
         for photo in photos[:count]:
             try:
                 client.delete_profile_photos(photo.file_id)
-                logger.info(f"Аватарка {photo.file_id} удалена успешно.")
+                logger.info(f"Авка {photo.file_id} удалена.")
             except FloodWait as e:
-                logger.warning(f"Превышен лимит запросов. Подождите {e.x} секунд.")
+                logger.warning(f"Флуд. Ждем {e.x} секунд.")
             except Exception as e:
-                logger.error(f"Ошибка при удалении аватарки {photo.file_id}: {e}")
+                logger.error(f"НЕ СМОГ УДАЛИТЬ ЧЗХ??? {photo.file_id}: {e}")
 
     except Exception as e:
-        logger.error(f"Ошибка при получении аватарок профиля: {e}")
+        logger.error(f"ОШИБКА при получении авок профиля: {e}")
 
 
 def download_photos():
     try:
         user = client.get_me()
 
-        # Создаём папку для сохранения фотографий
+        # Создаём папке для сохранения фоточег
         if not os.path.exists("pics"):
             os.makedirs("pics")
 
-        # Получаем аватарки профиля пользователя
+        # Получаем аватарке профиле пользователе
         photos = list(client.get_chat_photos(user.id))
 
         if not photos:
-            logger.info("Нет аватарок для скачивания.")
+            logger.info("НЕХУЙ СКАЧИВАТЬ, НЕТ НИЧЕ.")
             return
 
         for index, photo in enumerate(photos):
             try:
                 file_path = client.download_media(photo.file_id, file_name=f"pics/avatar_{index + 1}.jpg")
-                logger.info(f"Аватарка {photo.file_id} скачана: {file_path}")
+                logger.info(f"Пикча {photo.file_id} скачана: {file_path}")
             except Exception as e:
-                logger.error(f"Ошибка при скачивании аватарки {photo.file_id}: {e}")
+                logger.error(f"ОНО НЕ СКАЧИВАЕТСЯ {photo.file_id}: {e}")
 
     except Exception as e:
-        logger.error(f"Ошибка при скачивании аватарок профиля: {e}")
+        logger.error(f"Ошибке при скачивании авок: {e}")
 
 
 def show_info():
     info = """
-    Скрипт для очистки аватарок в Telegram.
+    Отчистка профиля ТГ от аватарок.
 
     Доступные команды:
-    1. Начать очистку - удаляет все или указанное количество аватарок.
-    2. Скачать аватарки - сохраняет все аватарки профиля в папку pics.
-    3. Инфо - показывает это сообщение.
-    4. Выход - завершает работу скрипта.
+    1. Начать очистку - сносит все или указанное кол-во аватарок.
+    2. Скачать аватарки - сохраняет все авки профиля в папку pics.
+    3. Инфо - показывает это сообщение(АХУЕТЬ).
+    4. Выход - завершает работу скрипта(НЕ МОЖЕТ БЫТЬ, АНБЕЛИВЕБЛЕ НАХУЙ).
 
+    P.S. 
+    Напиши 'q' находясь в любой вкладке для выхода в меню.
     Перед началом убедитесь, что ваш api_id и api_hash указаны правильно.
 
-    by MKultra69
+    by MKultra69 with l0ve
     """
     print(info)
 
@@ -118,31 +118,43 @@ def main_menu():
         print(Fore.BLUE + "2. Скачать аватарки" + Style.RESET_ALL)
         print(Fore.CYAN + "3. Инфо" + Style.RESET_ALL)
         print(Fore.RED + "4. Выход" + Style.RESET_ALL)
-        choice = input(Fore.WHITE + "Выберите действие: " + Style.RESET_ALL)
+        choice = input(Fore.WHITE + "Выбирай: " + Style.RESET_ALL)
 
         if choice == '1':
-            confirm = input(
-                Fore.YELLOW + Style.BRIGHT + "Очистить все аватарки? (Y/N): " + Style.RESET_ALL).strip().lower()
-            if confirm == 'y':
-                delete_all_photos()
-            elif confirm == 'n':
-                try:
-                    count = int(input(
-                        Fore.YELLOW + Style.BRIGHT + "Введите количество аватарок для удаления: " + Style.RESET_ALL))
-                    delete_some_photos(count)
-                except ValueError:
-                    print(Fore.RED + Style.BRIGHT + "Неверный ввод. Пожалуйста, введите число." + Style.RESET_ALL)
-            else:
-                print(Fore.RED + Style.BRIGHT + "Неверный ввод. Возврат в меню." + Style.RESET_ALL)
+            while True:
+                confirm = input(Fore.YELLOW + Style.BRIGHT + "Снести все(Y) авки или часть(N)?: " + Style.RESET_ALL).strip().lower()
+                if confirm == 'q':
+                    print(Fore.CYAN + "Идем в менюшку." + Style.RESET_ALL)
+                    break
+                elif confirm == 'y':
+                    delete_all_photos()
+                    break
+                elif confirm == 'n':
+                    while True:
+                        count_input = input(Fore.YELLOW + Style.BRIGHT + "Введи кол-во для удаления: " + Style.RESET_ALL).strip().lower()
+                        if count_input == 'q':
+                            print(Fore.CYAN + "Идем в менюшку." + Style.RESET_ALL)
+                            break
+                        try:
+                            count = int(count_input)
+                            delete_some_photos(count)
+                            break
+                        except ValueError:
+                            print(Fore.RED + Style.BRIGHT + "Какую то хуйню высрал юзерок, нормально напиши" + Style.RESET_ALL)
+                    if count_input == 'q':
+                        break
+                    break
+                else:
+                    print(Fore.RED + Style.BRIGHT + "Все хуйня, давай по новой." + Style.RESET_ALL)
         elif choice == '2':
             download_photos()
         elif choice == '3':
             show_info()
         elif choice == '4':
-            print(Fore.RED + Style.BRIGHT + "Выход из программы." + Style.RESET_ALL)
+            print(Fore.RED + Style.BRIGHT + "Прощай..." + Style.RESET_ALL)
             break
         else:
-            print(Fore.RED + Style.BRIGHT + "Неверный выбор. Попробуйте снова." + Style.RESET_ALL)
+            print(Fore.RED + Style.BRIGHT + "Все хуйня, давай по новой." + Style.RESET_ALL)
 
 
 if __name__ == "__main__":
@@ -152,7 +164,7 @@ if __name__ == "__main__":
         main_menu()
 
     except Exception as e:
-        logger.error(f"Ошибка: {e}")
+        logger.error(f"ЕГГОГ: {e}")
     finally:
         client.stop()
-        logger.info("Клиент остановлен.")
+        logger.info("Отключился(Клиент вырубился).")
